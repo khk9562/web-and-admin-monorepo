d18ac5d3696a7acfbb840069d470bb4b50e3def3
stella <khk9562@naver.com>
2024-06-26 22:05:15 +0900

    feat: SCSS 환경 세팅 및 테스트 완료

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import { Button } from "bootstrap";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <h1>Vite + React 사용자 단</h1>
-      <Button>버튼</Button>
+      <h1 className="hi">Vite + React 사용자 단</h1>
+      <Button variant="primary">버튼</Button>
     </>
   );
 }
--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "@monorepo-test-turbo/shared/styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
