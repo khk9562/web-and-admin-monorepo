6581d91aaea7f0f5e72209aefea3ead9ab737345
stella <khk9562@naver.com>
2024-06-26 22:49:53 +0900

    faet: CoreUI Admin 환경 기본세팅

--- apps/admin/src/main.jsx
+++ apps/admin/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "@monorepo-test-turbo/shared/styles/main.scss";
+import "@coreui/coreui/dist/css/coreui.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
